f00b8a311b1f4fb46009e2385a95f8fb5a8ca92c
Dmitry S <dsagal+git@gmail.com>
2018-09-28 22:39:33 -0400

    Big refactor and switch server-side to typescript.
    Move non-shopify-specific data-updating function to server/dataUpdates.ts.
    Collect all shopify-specific logic in server/index.ts.
    Add dependencies to allow typescript.
    Move frontend code to frontend subdirectory.

---
+++ server/dataUpdates.ts
@@ -0,0 +1,81 @@
+import {GristDocAPI} from 'grist-plugin-api';
+import {mapValues} from 'lodash';
+
+export interface ITableData {
+  [colId: string]: any[];
+}
+
+export interface IRecord {
+  [colId: string]: any;
+}
+
+export interface IPreparedUpdates {
+  changes: Map<number, IRecord>;    // Maps existing rowId to the updated record.
+  additions: IRecord[];             // Lists new records.
+}
+
+export interface IColumn {
+  id: string;
+  type: string;
+}
+
+/**
+ * Fetch table tableId. If it does not exists, creates one with the given columns.
+ */
+export async function fetchTable(docApi: GristDocAPI, tableId: string, columns: IColumn[]): Promise<ITableData> {
+  const tables = await docApi.listTables();
+  if (!tables.includes(tableId)) {
+    await docApi.applyUserActions([['AddTable', tableId, columns]]);
+  }
+  return docApi.fetchTable(tableId);
+}
+
+/**
+ * Given a keyId naming a column to serve as the primary key, and existing table data, prepares a
+ * set of additions and changes to the table.
+ */
+export function prepareUpdates(keyId: string, local: ITableData, fetched: IRecord[]): IPreparedUpdates {
+  // Maps rowId to updated Record.
+  const changes: Map<number, IRecord> = new Map();
+  const additions: IRecord[] = [];
+
+  // Maps key to index of record in local ITableData columns.
+  const localByKey: Map<any, number> =
+    new Map(local[keyId].map<[any, number]>((value, index) => [value, index]));
+
+  for (const rec of fetched) {
+    const key = rec[keyId];
+    const localIndex = localByKey.get(key);
+    if (localIndex) {
+      if (Object.keys(rec).some((k) => (rec[k] !== local[k][localIndex]))) {
+        changes.set(local.id[localIndex], rec);
+      }
+    } else {
+      additions.push(rec);
+    }
+  }
+  return {changes, additions};
+}
+
+/**
+ * Applies the set of updates returned by prepareUpdates().
+ */
+export async function applyUpdates(docApi: GristDocAPI, tableId: string, updates: IPreparedUpdates): Promise<void> {
+  const {changes, additions} = updates;
+  const userActions: any[][] = [];
+  if (additions.length) {
+    const rowIds = additions.map(() => null);   // All rowIds can be null.
+    userActions.push(['BulkAddRecord', tableId, rowIds, recordsToData(additions)]);
+  }
+  if (changes.size) {
+    userActions.push(['BulkUpdateRecord', tableId, changes.keys(), recordsToData(Array.from(changes.values()))]);
+  }
+  if (userActions.length) {
+    await docApi.applyUserActions(userActions);
+  }
+}
+
+function recordsToData<Record extends {[key: string]: any}>(records: Record[]): ITableData {
+  if (!records.length) { return {}; }
+  return mapValues(records[0], (val, key) => records.map((r) => r[key]));
+}
---
+++ server/index.ts
@@ -0,0 +1,141 @@
+// tslint:disable:no-console
+
+import * as grist from 'grist-plugin-api';
+import {sumBy} from 'lodash';
+import * as Shopify from 'shopify-api-node';
+import {applyUpdates, fetchTable, ITableData, prepareUpdates} from './dataUpdates';
+
+const storageApi: grist.Storage = grist.rpc.getStub('DocStorage@grist');
+const docApi: grist.GristDocAPI = grist.rpc.getStub('GristDocAPI@grist');
+
+const columns = [
+  {id: "ShopifyId",     type: "string"    },
+  {id: "ProcessedAt",   type: "DateTime"  },
+  {id: "OrderNumber",   type: "Text"      },
+  {id: "VariantTitle",  type: "Text"      },
+  {id: "SKU",           type: "Text"      },
+  {id: "Name",          type: "Text"      },
+  {id: "Title",         type: "Text"      },
+  {id: "Quantity",      type: "Numeric"   },
+  {id: "Amount",        type: "Numeric"   },
+  {id: "Taxes",         type: "Numeric"   },
+  {id: "Discount",      type: "Numeric"   },
+  {id: "RefundAmount",  type: "Numeric"   },
+  {id: "CreateAt",      type: "DateTime"  },
+  {id: "UpdatedAt",     type: "DateTime"  },
+];
+
+interface ILineItem {
+  ShopifyId: number;
+  ProcessedAt: Date;
+  OrderNumber: string;
+  VariantTitle: string;
+  SKU: string;
+  Name: string;
+  Title: string;
+  Quantity: number;
+  Amount: number;
+  Taxes: number;
+  Discount: number;
+  RefundAmount: number;
+  CreatedAt: Date;
+  UpdatedAt: Date;
+}
+
+interface IShopifyParams {
+  startDate: Date;
+  endDate: Date;
+}
+
+interface IUpdateResult {
+  updated: number;
+  added: number;
+}
+
+// Update table 'ShopifyItems'. If that table does not exist, creates it.
+export async function updateTable(params: IShopifyParams): Promise<IUpdateResult> {
+  const credentials = await getCredentials();
+  const shopify = new Shopify(credentials);
+  const localTable: ITableData = await fetchTable(docApi, 'ShopifyItems', columns);
+  const fetched: ILineItem[] = await fetchLineItems(shopify, params);
+
+  const updates = prepareUpdates('ShopifyId', localTable, fetched);
+  await applyUpdates(docApi, 'ShopifyItems', updates);
+  return {added: updates.additions.length, updated: updates.changes.size};
+}
+
+async function getCredentials(): Promise<Shopify.IPrivateShopifyConfig> {
+  const [credentials, apiSecret] = await Promise.all([
+    storageApi.getItem('shopify-credentials'),
+    storageApi.getItem('shopify-apiSecret'),
+  ]);
+  return {...credentials, apiSecret};
+}
+
+async function fetchLineItems(shopify: Shopify, params: IShopifyParams): Promise<ILineItem[]> {
+  const limit = 250;    // Max allowed per call.
+  const status = 'any';
+  const records: ILineItem[] = [];
+  for (let page = 1; ; page++) {
+    const orders = await shopify.order.list({...params, status, limit, page});
+    if (orders.length < limit) {
+      break;
+    }
+    for (const order of orders) {
+      processOrder(records, order);
+    }
+  }
+  return records;
+}
+
+function processOrder(records: ILineItem[], order: Shopify.IOrder): void {
+  for (const item of order.line_items) {
+    // Typescript types for IOrderLineItem are off or out of date for discount_allocations and tax_lines.
+    const discount = sumBy((item as any).discount_allocations, (r: any) => parseFloat(r.amount));
+    const taxes = sumBy(item.tax_lines as any, (r: any) => parseFloat(r.price));
+    const price = parseFloat(item.price);
+    records.push({
+      ShopifyId:    item.id,
+      ProcessedAt:  new Date(order.processed_at),
+      OrderNumber:  String(order.order_number),
+      VariantTitle: item.variant_title,
+      SKU:          item.sku,
+      Name:         item.name,
+      Title:        item.title,
+      Quantity:     item.quantity,
+      Amount:       price * item.quantity,
+      Taxes:        taxes,
+      Discount:     discount,
+      RefundAmount: 0,
+      CreatedAt:    new Date(order.created_at),
+      UpdatedAt:    new Date(order.updated_at),
+    });
+  }
+  for (const refund of order.refunds) {
+    for (const rli of refund.refund_line_items) {
+      const item = rli.line_item;
+      const discount = sumBy(item.discount_allocations, (r: any) => parseFloat(r.amount));
+      const taxes = sumBy(item.tax_lines, (r: any) => parseFloat(r.price));
+      const price = parseFloat(item.price);
+      records.push({
+        ShopifyId:    item.id,
+        ProcessedAt:  new Date(refund.processed_at),
+        OrderNumber:  String(order.order_number),
+        VariantTitle: item.variant_title,
+        SKU:          item.sku,
+        Name:         item.name,
+        Title:        item.title,
+        Quantity:     -item.quantity,
+        Amount:       0,
+        Taxes:        taxes,
+        Discount:     0,
+        RefundAmount: price * item.quantity - discount,
+        CreatedAt:    new Date(refund.created_at),
+        UpdatedAt:    new Date(refund.processed_at),    // no separate value for this
+      });
+    }
+  }
+}
+
+grist.rpc.registerFunc('updateTable', updateTable);
+grist.ready();
